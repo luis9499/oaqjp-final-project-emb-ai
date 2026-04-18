126cd0fd215bad19d3697799db0f1026ab149163
luis9499 <lhh9499@gmail.com>
2026-04-18 15:29:29 -0500

    Add files via upload

--- EmotionDetection/__init__.py
+++ EmotionDetection/__init__.py
@@ -1,1 +1,10 @@
+"""
+EmotionDetection Package
+A package for detecting emotions in text using Watson NLP
+"""
 
+from .emotion_detection import emotion_detector
+
+__version__ = "1.0.0"
+__author__ = "Emotion Detection Team"
+__all__ = ["emotion_detector"]
